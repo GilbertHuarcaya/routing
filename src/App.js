aa737835ee13f3b7f6cfaee4fc1836c7d2c46c1b
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-30 20:12:55 -0500

    aplied routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Pricing from './components/Pricing';
-
+import "./App.css";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Pricing />
-    </div>
+    <section className="app">
+      <nav className="nav">
+        <Link
+          to="/home"
+          className={useLocation().pathname === "/home" ? "pressed" : ""}>
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={useLocation().pathname === "/about" ? "pressed" : ""}>
+          About
+        </Link>
+        <Link
+          to="/pricing"
+          className={useLocation().pathname === "/pricing" ? "pressed" : ""}>
+          Pricing
+        </Link>
+      </nav>
+      <Outlet />
+    </section>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./components/Home";
+import About from "./components/About";
+import Pricing from "./components/Pricing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
