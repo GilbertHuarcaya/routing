88d4b444d37524787f1a8a9a02dbf3c0ffb06b40
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-11-30 20:13:55 -0500

    updated components data and styles

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,18 @@
+import "./styles.css";
 const Home = () => (
-	<>
-		<section className="home">
-			<h1>Home</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, tempora. Velit commodi numquam maxime vitae eaque, in debitis ipsa doloribus quidem quia, officia ullam aperiam voluptatum! Temporibus nulla molestias doloribus commodi minus aperiam ex distinctio voluptatibus, nam neque ea, inventore odit tempore? Error dolorum eaque rerum modi rem, nesciunt necessitatibus?
-			</p>
-		</section>
-	</>
+  <>
+    <section className="home">
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, tempora.
+        Velit commodi numquam maxime vitae eaque, in debitis ipsa doloribus
+        quidem quia, officia ullam aperiam voluptatum! Temporibus nulla
+        molestias doloribus commodi minus aperiam ex distinctio voluptatibus,
+        nam neque ea, inventore odit tempore? Error dolorum eaque rerum modi
+        rem, nesciunt necessitatibus?
+      </p>
+    </section>
+  </>
 );
 
 export default Home;
